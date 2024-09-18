fc62746dc1d9de518dac41a31ea6407c04136f20
SciMind2460 <saarthkarkera@gmail.com>
2024-09-18 17:41:07 +0530

    Fixed bugs in preprocessor.py relating to extremely high preprocessing numbers, escaping them

--- preprocessor.py
+++ preprocessor.py
@@ -6,14 +6,18 @@ import re
 import string 
 import inflect 
 
+# Uses the Inflect library to convert text to numbers
 p = inflect.engine()
 def convert_number_to_text(text):
     temp_str = text.split()
     new_string = []
     for word in temp_str:
         if word.isdigit():
-            temp = p.number_to_words(word)
-            new_string.append(temp)
+            try:
+                temp = p.number_to_words(word)
+                new_string.append(temp)
+            except Exception as e:
+                new_string.append(word)
         else:
             new_string.append(word)
     temp_str = ' '.join(new_string)
@@ -21,12 +25,12 @@ def convert_number_to_text(text):
 
 def preprocessed_text(dataset):
     dataset = dataset.lower()
-    dataset = convert_number_to_text(dataset)
     dataset = dataset.translate(str.maketrans('', '', string.punctuation))
-    list_of_words = dataset.split(' ')
+    dataset = convert_number_to_text(dataset)
+    list_of_words = dataset.split()
     return list_of_words
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     preprocessor_sample = "2 people walked into a bar. Manav says hello to me."
     process_sample = preprocessed_text(preprocessor_sample)
     print(process_sample)
