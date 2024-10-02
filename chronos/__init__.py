3af13c28b6f4a1275dca2b90e99f8d08200b2704
Kurt Heiritz (pseudo) <saarthkarkera@gmail.com>
2024-10-02 17:18:23 +0530

    Fixed a mismatch in naming of files - preprocessor vs utils

--- chronos/__init__.py
+++ chronos/__init__.py
@@ -1,5 +1,5 @@
-from .preprocessor import preprocess_text, convert_number_to_text
-from .transformer_model import TextGenerationTransformer, generator, trainer, pre_trained_model
+from .utils import preprocess_text, convert_number_to_text, preprocess_function, shift_tokens
+from .transformer_model import TextGenerationTransformer, generator, trainer, pre_trained_model, tokenizer
 
 __all__ = [
     'preprocess_text',
