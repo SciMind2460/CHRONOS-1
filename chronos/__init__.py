fb53290859f49b153dde2570596df12eaac68ccb
Kurt Heiritz (pseudo) <saarthkarkera@gmail.com>
2024-10-02 12:53:58 +0530

    Update __init__.py

--- chronos/__init__.py
+++ chronos/__init__.py
@@ -1,8 +1,8 @@
 from .preprocessor import preprocess_text
-from .transformer_model import TextGenerationTransformer, pre_trained_model
+from .transformer_model import TextGenerationTransformer, generator
 
 __all__ = [
     'preprocess_text',
     'TextGenerationTransformer',
-    'pre_trained_model'
+    'generator'
 ]
